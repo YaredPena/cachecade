50b89733956797c01c34547e9110e849b853fd67
Yared <penayared16@gmail.com>
2025-03-14 10:53:13 -0400

    serverUp

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log("hi");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const routes = require('./routes/routes');
+const APP_PORT = process.env.APP_PORT || 7050
+
+app.use(express.json());
+
+app.listen(APP_PORT, () => {
+    console.log(`engine is running! http://localhost:${APP_PORT}`);
+});
+
+app.use('/api', routes);
