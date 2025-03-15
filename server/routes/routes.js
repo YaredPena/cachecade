136b997ebbc9b1f6b085e633df7de3fd4fafe2ad
Yared <penayared16@gmail.com>
2025-03-15 14:36:49 -0400

    CRUDendpoints

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,52 +2,83 @@ const express = require('express');
 const model = require('../models/apiKeyModel');
 const router = express.Router();
 
+/* I just wanted to test something
+const testKey = [
+    {   id: 1, 
+        name: 'testkey', 
+        key: 'asohfirbevneonvoirenvaoer'
+    }
+]
+
+*/
+
 router.get('/getAllKeys', async (req, res) => {
-    
-    
     try{
-       await res.send({ message: 'getAllkey'});
+        const data = await model.find();
+        res.json(data);
     } catch(error) {
         res.status(500).send(`Could not GET keys: ${error}`);
     }
 });
 
 router.get('/getOneKey/:id', async (req,res) => {
-
     try{
-        await res.send({ message: 'getOnekey'});
+        const data = await model.findById(req.params.id);
+        res.json(data)
     } catch(error) {
         res.status(500).send(`Could not GET key: ${error}`);
     }
 });
 
+// req.body. ==> as we're making a request (req), we're stitching for example the keyname to a body to be a response for something later.
 router.post('/postKey', async (req,res) => {
+    const key = new Model({
+        keyName: req.body.keyName,
+        apiKey: req.body.apikey,
+        description: req.body.description,
+        updatedAt: req.body.updatedAt,
+        createdAt: req.body.createdAt
+    });
+
 
     try{
-        await res.send({ message: 'postKey'}); 
+        const saveKey = await key.save();
+        res.satus(200).json(saveKey)
     } catch(error) {
         res.status(400).send(`Could not POST key: ${error}`);
     }
 });
 
+// here's we're assigning id to be a parameter in our request this means we find things using their id
+// so that we can update that key with a new one (should our old key expire.. I haven't met a service that expires api keys though)
 router.patch('/patchKey/:id', async (req, res) => {
+    const id = req.params.id;
+    const updatedKey = req.body;
+    const options = { new: true };
+        
+    const result = await Model.findByIdAndUpdate(
+        id, 
+        updatedKey, 
+        options
+    )   
 
     try{
-        await res.send({ message: 'patchKey'});
+        await res.send(result);
     } catch(error) {
         res.status(400).send(`Could not PATCH key: ${error}`);
     }
 });
 
+// we're find the api key by id here and deleting based off of that. 
+// then we're sending that the {key.title} <-- this is dynamic has been deleted to the console.
 router.delete('/deleteKey/:id', async (req, res) => {
-
     try{
-        await res.send({ message: 'deleteKey'});
+        const id = req.params.id;
+        const key = await Model.findByIdAndDelete(id)
+        res.send(`${key.title} is deleted..`)
     } catch(error) {
         res.status(400).send(`Could not DELETE key: ${error}`);
     }
 });
-
-// alright now make them actually do what the should be doing 
 
 module.exports = router;
