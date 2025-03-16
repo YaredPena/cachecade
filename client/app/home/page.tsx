cd489021984669998eacbfc54237cbae957a5a1b
Yared <penayared16@gmail.com>
2025-03-16 15:00:37 -0400

    serviceworks+endpointsworkBackend+FrontendConnected

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -3,27 +3,38 @@
 import React, {useState} from 'react';
 import { fetchData } from '@/lib/apiService'
 
+const HomePage = () => {
+
 const [apiKeys, setApiKeys] = useState(null);
 const [loading, setLoading] = useState(false);
 const [error, setError] = useState(null);
 
-const HomePage = () => {
-
     const handleFetchData = async () => {
+        setLoading(true);
+        setError(null);   
+        try {
+          const data = await fetchData(`${process.env.NEXT_PUBLIC_API_URL}/getAllKeys`);
+          setApiKeys(data);
 
-        try{
-            
-            const data = await fetchData(`${process.env.NEXT_PUBLIC_API_URL}`);
-
-        } catch(error) {
-
-        } finally{
-
+        } catch (error) {
+          setError(null); /// this should probably not be null I should actually out some type of errors
+        } finally {
+          setLoading(false);
         }
-    }
+      };
 
     return(
-        <h1 className="underline">hi homepage</h1>
+    <div>
+      <button onClick={handleFetchData}>Fetch Data</button>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {apiKeys && (
+        <div>
+          <h3>API Keys:</h3>
+          <pre>{JSON.stringify(apiKeys, null, 2)}</pre>
+        </div>
+      )}
+    </div>
     );
 };
 
