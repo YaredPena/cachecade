f96e180173c590c5ee5f84cda43cd9ea9d33ed18
Yared <penayared16@gmail.com>
2025-03-16 03:26:18 -0400

    figuredoutlinkingbetweenpages

---
+++ client/app/home/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React, {useState} from 'react';
+
+const HomePage = () => {
+
+    return(
+        <h1 className="underline">hi homepage</h1>
+    );
+};
+
+export default HomePage;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <h1 className="underline"> hello world! </h1>
+    <Link href="/home"> HomePage </Link>
+
   );
 }
